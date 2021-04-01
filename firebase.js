ee5a7db8d79a87bdd009c456c45647e868ec0704
BuiHung1612 <you@example.com>
2021-04-01 20:50:46 +0700

    firebase

--- firebase.js
+++ firebase.js
@@ -4,5 +4,6 @@ const firebaseConfig = {
     projectId: "signalclone-5599f",
     storageBucket: "signalclone-5599f.appspot.com",
     messagingSenderId: "1059916117344",
-    appId: "1:1059916117344:web:938951c9d6253453255a05";
+    appId: "1:1059916117344:web:938951c9d6253453255a05"
   };
+  ///ads
